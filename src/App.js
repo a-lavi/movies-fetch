6854ae3ad724a664db102c6fd630318438223013
a-lavi <ali.rahmani.63@icloud.com>
2022-07-06 21:33:11 +0200

    add search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [ searchWord, setSearchWord]= useState('destiny')
+  const readMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=b4fa904b6dd6ec161038cb628ce5ce65&language=en-US&query=${searchWord}&page=1&include_adult=false`
+    )
+    const result = await response.json()
+    console.log(result)
+    setMovies(() => result.results)
+  }
+  const moviesList = movies.map((movie, index) => {
+    return (
+      <li key={index}>
+        <h3>{movie.title}</h3>
+        <div>
+          {movie.overview}
+        </div>
+      </li>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movies</h1>
+      <button onClick={readMovies}>Search</button>
+
+      <div>
+        <ul>
+          {moviesList}
+        </ul>
+      </div>
     </div>
   );
 }
